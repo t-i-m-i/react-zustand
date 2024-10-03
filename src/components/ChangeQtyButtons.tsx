5d98937c3ad53d064658ac4b2af1385846845cc1
Tymoteusz Bak <timi@timiportfolio.com>
2024-10-03 23:56:04 +0200

    change buttons improvements

--- src/components/ChangeQtyButtons.tsx
+++ src/components/ChangeQtyButtons.tsx
@@ -7,6 +7,14 @@ interface Props {
 
 export default function ChangeQtyButtons({ productId }: Props) {
 
+  // - This approach extracts all the properties (getProductById, incQty, decQty) directly from the store without any selector.
+  // - It might cause unnecessary re-renders if any state in the store changes, even if those specific functions (getProductById, incQty, decQty) remain unchanged.
+  // - This can affect performance if your store contains other reactive state elements that update frequently but aren't relevant to these functions.
+  // const { getProductById, incQty, decQty } = useStore();
+
+  // - This approach uses a selector function to explicitly pick the relevant state
+  // - This will optimize the component by only re-rendering when the selected parts change
+  // - It's more efficient in avoiding unnecessary renders caused by other unrelated state changes in the store.
   const { getProductById, incQty, decQty } = useStore(
     useShallow((state) => ({
       getProductById: state.getProductById,
